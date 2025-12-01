c184d56e378ac71492f864386ee4a1493ac420aa
klisman <klisman.gomes.silva@gmail.com>
2025-12-01 11:41:37 -0300

    Adicionando a pasta imagens. Atualizando o App.jsx e tirando o tailwind completamente para usar apenas o CSS no arquivo App.css

--- frontend/postcss.config.cjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('postcss-load-config').Config} */
-module.exports = {
-  plugins: {
-    // Usamos o nome de pacote exato sugerido pelo erro.
-    '@tailwindcss/postcss': {}, 
-    // O autoprefixer é necessário para compatibilidade de navegadores.
-    'autoprefixer': {},
-  }
-}
